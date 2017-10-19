56d19e16f6d9af14abc88f7143c444ddd4d56ba5
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-10-19 17:20:15 +0300

    Added working no -compression algorithm

--- VisualStudio2017/ProjectAlexandru/ProjectAlexandru/Algo.h
+++ VisualStudio2017/ProjectAlexandru/ProjectAlexandru/Algo.h
@@ -1,18 +1,72 @@
 #pragma once
 
 #include <string>
+#include <fstream>
 #include <opencv2/opencv.hpp>
+
+using namespace std;
 
 namespace Algo
 {
+	std::ifstream::pos_type filesize(const char* filename)
+	{
+		std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+		return in.tellg();
+	}
+
 	std::string compress(cv::Mat& rawImage)
 	{
-		return "";
+		std::string fileName = "compressedImage.dat";
+		ofstream OutFile;
+		OutFile.open(fileName, ios::out | ios::binary | ios::trunc);
+		OutFile.write((char*)&rawImage.rows, sizeof(rawImage.rows));
+		OutFile.write((char*)&rawImage.cols, sizeof(rawImage.cols));
+
+		uint8_t* pixelsPtr = (uint8_t*)rawImage.data;
+		for (int i = 0; i < rawImage.rows; i++)
+		{
+			for (int j = 0; j < rawImage.cols; j++)
+			{
+				uint8_t blue = pixelsPtr[i*rawImage.cols*rawImage.channels() + j*rawImage.channels() + 0];
+				OutFile.write((char*)&blue, sizeof(blue));
+				uint8_t green = pixelsPtr[i*rawImage.cols*rawImage.channels() + j*rawImage.channels() + 1];
+				OutFile.write((char*)&green, sizeof(green));
+				uint8_t red = pixelsPtr[i*rawImage.cols*rawImage.channels() + j*rawImage.channels() + 2];
+				OutFile.write((char*)&red, sizeof(red));
+			}
+		}
+
+		OutFile.close();
+		return fileName;
 	}
 
 	cv::Mat uncompress(std::string & uncompressedImagePath)
 	{
-		cv::Mat uncompressedImage;
+		cv::Mat uncompressedImage(0, 0, CV_8UC3);
+		ifstream InFile;
+		InFile.open(uncompressedImagePath, ios::in | ios::binary);
+		InFile.read((char*)&uncompressedImage.rows, sizeof(uncompressedImage.rows));
+		InFile.read((char*)&uncompressedImage.cols, sizeof(uncompressedImage.cols));
+		uncompressedImage.data = new uint8_t[uncompressedImage.rows * uncompressedImage.cols * 3];
+		int elemIdx = 0;
+		for (int i = 0; i < uncompressedImage.rows; i++)
+		{
+			for (int j = 0; j < uncompressedImage.cols; j++)
+			{
+				for (size_t channels = 0; channels < 3; ++channels)
+				{
+					uint8_t data;
+					InFile.read((char*)&data, sizeof(uint8_t));
+					uncompressedImage.data[elemIdx] = data;
+					++elemIdx;
+				}
+			}
+		}
+
+		InFile.close();
+
+		cout << "File size is: " << filesize(uncompressedImagePath.c_str()) << endl;
+
 		return uncompressedImage;
 	}
 }
--- VisualStudio2017/ProjectAlexandru/ProjectAlexandru/Utils.h
+++ VisualStudio2017/ProjectAlexandru/ProjectAlexandru/Utils.h
@@ -5,8 +5,54 @@
 
 namespace Utils
 {
-	void validate(const cv::Mat& actual, const cv::Mat& expected, const std::string& message = "")
+	void validate(const cv::Mat& expected, const cv::Mat& actual)
 	{
-		std::cout << "TEST FAILED: " << message << std::endl;
+		if (expected.rows != actual.rows)
+		{
+			std::cout << "TEST FAILED: Number of rows does not match expected " << expected.rows << " and got " << actual.rows << std::endl;
+			return;
+		}
+
+		if (actual.cols != expected.cols)
+		{
+			std::cout << "TEST FAILED: Number of columns does not match expected " << expected.rows << " and got " << actual.rows << std::endl;
+			return;
+		}
+
+		int expectedChannels = expected.channels();
+		int actualChannels = actual.channels();
+		if (actualChannels != expectedChannels)
+		{
+			std::cout << "TEST FAILED: Number of channels does not match expected " << expectedChannels << " and got " << actualChannels << std::endl;
+			return;
+		}
+
+		uint8_t* actualPixelsPtr = (uint8_t*)actual.data;
+		if (actualPixelsPtr == nullptr)
+		{
+			std::cout << "TEST FAILED: Actual image data is invalid" << std::endl;
+			return;
+		}
+		uint8_t* expectedPixelsPtr = (uint8_t*)expected.data;
+		if (expectedPixelsPtr == nullptr)
+		{
+			std::cout << "TEST FAILED: Expected image data is invalid" << std::endl;
+			return;
+		}
+
+		for (int i = 0; i < actual.rows; i++)
+		{
+			for (int j = 0; j < actual.cols; j++)
+			{
+				if ((actualPixelsPtr[i*actual.cols*actualChannels + j*actualChannels + 0] != expectedPixelsPtr[i*expected.cols*expectedChannels + j*expectedChannels + 0]) ||
+					(actualPixelsPtr[i*actual.cols*actualChannels + j*actualChannels + 1] != expectedPixelsPtr[i*expected.cols*expectedChannels + j*expectedChannels + 1]) || 
+					(actualPixelsPtr[i*actual.cols*actualChannels + j*actualChannels + 2] != expectedPixelsPtr[i*expected.cols*expectedChannels + j*expectedChannels + 2]))
+				{
+					std::cout << "TEST FAILED : The pixel at (" << i << ", " << j << ") is different from source" << std::endl;
+					return;
+				}
+			}
+		}
+		std::cout << "TEST SUCCESFULL" << std::endl;
 	}
 }
